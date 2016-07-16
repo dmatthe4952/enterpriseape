848fa428db9820f82a9f6ca6b557214b48285c20
davematthews <dmatthe@yahoo.com>
2016-07-16 11:56:19 +0000

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :invoices
-  resources :invoices
+  root 'invoices#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
