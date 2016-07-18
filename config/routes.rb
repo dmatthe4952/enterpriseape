58d9936596f34347f3f03d87c3663e2ad46e89ff
davematthews <dmatthe@yahoo.com>
2016-07-18 00:11:26 +0000

    add CSV Uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
+  resources :companies do
+    collection { post :import}
+  end
   devise_for :users
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/features'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
 
   resources :invoices do
