a2b36e76d82d27700ea4d0a3953d81f374e8f59b
david <dmatthe@yahoo.com>
2016-07-18 13:51:04 -0500

    removing hardcoded credentials from setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address                       => 'smtp.sendgrid.net',
   :port                          => '587',
   :authentication                => :plain,
-  :user_name                     => 'app53692422@heroku.com',
-  :password                      => 'z0zzlmbj4742',
+  :user_name                     => ENV["SENDGRID_USER_NAME"],
+  :password                      => ENV["SENDGRID_PASSWORD"],
   :domain                        => 'heroku.com',
   :enable_starttls_auto          => true
   }
