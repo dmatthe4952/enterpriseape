2111c406046f1d97e628bacf279a6f12256cdc7c
david <dmatthe@yahoo.com>
2016-07-22 17:40:19 -0500

    add dropdown using db call

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721133124) do
+ActiveRecord::Schema.define(version: 20160722173252) do
 
   create_table "companies", force: :cascade do |t|
     t.string  "name"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20160721133124) do
     t.datetime "date"
     t.string   "company"
     t.decimal  "tax"
-    t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status_type"
+    t.integer  "employee_id"
   end
 
   create_table "users", force: :cascade do |t|
